aab71d27a6a0f9d4edfe183efd769c6a18bd45b9
IuliaNeuc <Lulia.Neuc@edu.sait.ca>
2023-11-09 15:12:26 -0700

    Finished assignment 7 week 7

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -183,6 +183,20 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Add new item to your list</p>  {/*Render StudentInfo */}
         </Link>
+        <Link 
+         href="/week7" 
+         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+         target="_blank"
+         rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Assignment Week 7{' '} {/* Displaying the heading: CPRG 306: Web Development 2 - Assignment 7 Week 7  */}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Add new item to your list</p>  {/*Render StudentInfo */}
+        </Link>
       </div>
     </main>
   )
---
+++ cprg306-assignments/app/week7/item.js
@@ -0,0 +1,21 @@
+// Define a functional component named 'Item that accepts three props: name, quantity, category
+export default function Item({name, quantity, category, onSelect})
+{
+
+    const handleClick = () => {
+        onSelect(name);
+    };
+
+    return(
+        // Render a list item element with specific styling classes for appearance
+        <li className="border p-4 my-2 rounded-lg shadow-md" onClick={handleClick}> 
+            <div className="flex justify-between"> {/* Create a flex container with space between its children */}
+                <div className="flex flex-col">{/* Create a flex container for the item name and category */}
+                    <span className="text-lg font-semibold">{name}</span>
+                    <span className="text-gray-500">{category}</span>
+                </div>
+                <span className="text-xl font-semibold">{quantity}</span> {/* Render the item quantity with a larger font size and bold style on the right side. */}
+            </div> 
+        </li>
+    );
+}
